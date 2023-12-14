bf0377bafa599e3ad7b18f9f41f6ad2461488156
sky <1255067427@qq.com>
2023-12-14 10:20:26 +0800

    搭建环境

---
+++ src/main/java/com/sky/skymusicsystem/SkyMusicSystemApplication.java
@@ -0,0 +1,16 @@
+package com.sky.skymusicsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sky
+ */
+@SpringBootApplication
+public class SkyMusicSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyMusicSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sky/skymusicsystem/SkyMusicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.skymusicsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyMusicSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
